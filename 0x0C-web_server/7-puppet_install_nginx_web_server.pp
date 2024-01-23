1258b5215367baa665c7812ae1e9b9e72bf92211
Temiedayo <olalekan.temidayo.916@gmail.com>
2024-01-23 14:30:58 -0800

    web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Configure server using puppet
+
+# Defines a Puppet class called nginx_server that 
+
+#  Summarizes the configuration for the Nginx server.
+class nginx_server {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+#  Manages the Nginx service.
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    require => Package['nginx'],
+  }
+
+# Manages the Nginx configuration file located at /etc/nginx/sites-available/default.
+  file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => "
+      server {
+        listen      80 default_server;
+        listen      [::]:80 default_server;
+        root        /var/www/html;
+        index       index.html index.htm;
+
+        location / {
+          return 200 'Hello World!';
+        }
+
+        location /redirect_me {
+          return 301 http://cuberule.com/;
+        }
+      }
+    ",
+    notify => Service['nginx'],
+  }
+}
+#  Includes the nginx_server class, ensuring that it gets applied.
+include nginx_server
+
